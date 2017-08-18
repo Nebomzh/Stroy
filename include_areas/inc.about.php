0d957aa23817a4901920426b967b9f939cfdd945
Alexander Morozov <c-a-h-e-k2006@yandex.ru>
2017-08-18 22:03:15 +0500

    Слайды с утеплением пола, балкона и цоколя! Да! три слайда за раз! Ещё и png на jpg где можно заменил!

--- include_areas/inc.about.php
+++ include_areas/inc.about.php
@@ -1,100 +1,140 @@
 <section id="floor">
 <div id="flooranchor" class="anchor"></div>
 <div class="container">
+	<div class="flexcol">
 	<div class="box">
 		<div class="center">
 			<h2>Утепление пола</h2>
 			<p style="text-align: justify;">
-						 Утепление бетонного пола очень важно для создания уюта и тепла в доме, особенно если квартира расположена на первом этаже. Деревянные полы также требуют утепления. Например, в частном доме иногда даже хорошо смонтированное покрытие не дает полной гарантии сохранения тепла, а значит сэкономить на отоплении не удастся. Чтобы избежать таких ситуаций и жить в комфорте, разумным решением будет утепление пола при помощи экструзионного пенополистирола XPS<br>
+						 Утепление бетонного пола очень важно для создания уюта и тепла в доме, особенно если квартира расположена на первом этаже. Деревянные полы также требуют утепления. Например, в частном доме иногда даже хорошо смонтированное покрытие не дает полной гарантии сохранения тепла, а значит сэкономить на отоплении не удастся. Чтобы избежать таких ситуаций и жить в комфорте, разумным решением будет утепление пола при помощи экструзионного пенополистирола XPS.<br>
+						 Хорошие теплоизолирующие свойства и высокая прочность материала делают его практически незаменимым в конструкциях теплого пола.
 			</p>
 		</div>
 		 <!--             <div class="big-gap"></div> -->
+	</div>
+	</div>
+</div>
+</section>
+<section id="floor2">
+<div class="container">
+	<div class="box">
 		<div id="pricing-table" class="row">
 			<div class="col-sm-12">
 				<ul class="plan">
-					<li class="plan-price"><img src="/upload/iblock/aef/aefef3973cf94341d8d2d2fec548108f.jpg" class="img-responsive"></li>
+					<li class="plan-name"><p>ПРЕИМУЩЕСТВА</p></li>
 					<li>
-					<p style="text-align: justify;">
-						 
- <i class="icon-ok icon-md icon-color2"></i>Удобно использовать – листы XPS нужно просто уложить на пол;<br>
- <i class="icon-ok icon-md icon-color2"></i>Быстро утеплить – процесс утепления займет совсем немного времени из-за простоты использования;<br>
- <i class="icon-ok icon-md icon-color2"></i>Прослужит долгое время – до 50 лет;<br>
- <i class="icon-ok icon-md icon-color2"></i>Экономия на услугах ЖКХ – хорошая теплоизоляция позволит значительно сэкономить на отоплении.
-					</p>
- </li>
+					<div class="flexcol">
+<span><i class="icon-ok icon-md icon-color2"></i><p>Удобно использовать – листы XPS нужно просто уложить на пол;</p></span>
+<span><i class="icon-ok icon-md icon-color2"></i><p>Быстро утеплить – процесс утепления займет совсем немного времени из-за простоты использования;</p></span>
+<span><i class="icon-ok icon-md icon-color2"></i><p>Прослужит долгое время – до 50 лет;</p></span>
+<span><i class="icon-ok icon-md icon-color2"></i><p>Экономия на услугах ЖКХ – хорошая теплоизоляция позволит значительно сэкономить на отоплении.</p></span>
+					</div>
+					</li>
 					<li>
 					<div class="videoWrapper">
-						 <iframe width="640" height="480" src="//www.youtube.com/embed/3Sl0_nNN4vo?rel=0&autoplay=0&showinfo=0&controls=0&HD=1" frameborder="0" allowfullscreen></iframe>
+							<iframe width="640" height="480" src="//www.youtube.com/embed/3Sl0_nNN4vo?rel=0&autoplay=0&showinfo=0&controls=0&HD=1" frameborder="0" allowfullscreen></iframe>
+							<p>Смотрите ролик об утеплении пола</p>
 					</div>
- </li>
+					</li>
 				</ul>
 			</div>
 		</div>
 	</div>
 </div>
 </section>
+<div style="text-align: center">
+	<a href="https://www.stroyxpress.ru/catalog/uteplitel/ekstrudirovannyy_penopolistirol/" class="btn btn-primary btn-lg">Перейти в каталог</a>
+</div>
 <section id="balkon">
 <div id="balkonanchor" class="anchor"></div>
 <div class="container">
+	<div class="flexcol">
 	<div class="box">
 		<div class="center">
 			<h2>Утепление балкона</h2>
+			<p style="text-align: justify;">
+					Из балкона можно сделать комфортное место для отдых или превратить его в отдельную комнату, хорошо утеплив его. XPS идеален для этой цели! Он обладает хорошими теплоизолирующими свойствами, даже при минимальной толщине. Так, 50 мм утеплителя хватит, чтобы надежно защитить балкон и помещения от холода снаружи, в большинстве регионов России. К тому же, XPS обладает минимальным водопоглащением, что гарантирует долговечность материала и сохранение теплоизолирующих свойств на протяжении 40-50 лет.
+			</p>			
 		</div>
 		 <!--             <div class="big-gap"></div> -->
+	</div>
+	</div>
+</div>
+</section>
+<section id="balkon2">
+<div class="container">
+	<div class="box">
 		<div id="pricing-table" class="row">
 			<div class="col-sm-12">
 				<ul class="plan">
-					<li class="plan-price"><img src="/upload/iblock/642/642f6ab151d1a8ecacd320df3deab66f.jpg" class="img-responsive"></li>
+					<li class="plan-name"><p>ПРЕИМУЩЕСТВА</p></li>
 					<li>
-					<p style="text-align: justify;">
-						 Из балкона можно сделать комфортное место для отдых или превратить его в отдельную комнату, хорошо утеплив его. XPS идеален для этой цели! Он обладает хорошими теплоизолирующими свойствами, даже при минимальной толщине. Так, 50 мм утеплителя хватит, чтобы надежно защитить балкон и помещения от холода снаружи, в большинстве регионов России. К тому же, XPS обладает минимальным водопоглащением, что гарантирует долговечность материала и сохранение теплоизолирующих свойств на протяжении 40-50 лет.<br>
- <i class="icon-ok icon-md icon-color2"></i>Утеплитель не «съедает» большое количество площади своим размером. Для надежного утепления балкона достаточно слоя в 50 мм;<br>
- <i class="icon-ok icon-md icon-color2"></i>Сохраняет теплоизолирующих свойства на протяжении 40-50 лет;<br>
- <i class="icon-ok icon-md icon-color2"></i>Не боится влаги;<br>
- <i class="icon-ok icon-md icon-color2"></i>Быстрый и легкий монтаж.
-					</p>
- </li>
+						<div class="flexcol">
+<span><i class="icon-ok icon-md icon-color2"></i><p>Утеплитель не «съедает» большое количество площади своим размером.</p></span>
+<span><i class="icon-ok icon-md icon-color2"></i><p>Для надежного утепления балкона достаточно слоя в 50&nbspмм;</p></span>
+<span><i class="icon-ok icon-md icon-color2"></i><p>Сохраняет теплоизолирующих свойства на протяжении 40-50 лет;</p></span>
+<span><i class="icon-ok icon-md icon-color2"></i><p>Не боится влаги;</p></span>
+<span><i class="icon-ok icon-md icon-color2"></i><p>Быстрый и лёгкий монтаж.</p></span>
+						</div>
+					</li>
 					<li>
 					<div class="videoWrapper">
 						 <iframe width="640" height="480" src="//www.youtube.com/embed/EGJbs7Qm6gc?rel=0&autoplay=0&showinfo=0&controls=0&HD=1" frameborder="0" allowfullscreen></iframe>
+						 <p>Смотрите ролик об утеплении ,балкона</p>
 					</div>
- </li>
+					</li>
 				</ul>
 			</div>
 		</div>
 	</div>
 </div>
 </section>
+<div style="text-align: center">
+	<a href="https://www.stroyxpress.ru/catalog/uteplitel/ekstrudirovannyy_penopolistirol/" class="btn btn-primary btn-lg">Перейти в каталог</a>
+</div>
 <section id="podval">
 <div id="podvalanchor" class="anchor"></div>
 <div class="container">
+	<div class="flexcol">
 	<div class="box">
 		<div class="center">
 			<h2>Утепление цоколя</h2>
+					<p style="text-align: justify;">
+						 Для качественной и надёжной теплоизоляции цоколя лучше всего подходит экструзионный пенопоплистирол. XPS представляет собой вспененный под высоким давлением полимер, заполненный мельчайшими воздушными пузырьками. Он абсолютно устойчив к воздействию влаги, грибка и плесени, ему не вредят ни грызуны, ни корни растений, а срок его службы превышает 50 лет.<br>
+						 Кроме этого – это надежная изоляция «мостиков холода» в здании, через которые тепло просачивается наружу.
+					</p>
 		</div>
 		 <!--             <div class="big-gap"></div> -->
+	</div>
+	</div>
+</section>
+<section id="podval2">
+<div class="container">
+	<div class="box">
 		<div id="pricing-table" class="row">
 			<div class="col-sm-12">
 				<ul class="plan">
-					<li class="plan-price"><img src="/upload/iblock/6c1/6c1116b9d3accf13a07f28aec82f1e64.jpg" class="img-responsive"></li>
+					<li class="plan-name"><p>ПРЕИМУЩЕСТВА</p></li>
 					<li>
-					<p style="text-align: justify;">
-						 Для качественной и надёжной теплоизоляции цоколя лучше всего подходит экструзионный пенопоплистирол. XPS представляет собой вспененный под высоким давлением полимер, заполненный мельчайшими воздушными пузырьками. Он абсолютно устойчив к воздействию влаги, грибка и плесени, ему не вредят ни грызуны, ни корни растений, а срок его службы превышает 50 лет.<br>
-						 Кроме этого – это надежная изоляция «мостиков холода» в здании, через которые тепло просачивается наружу.<br>
- <i class="icon-ok icon-md icon-color2"></i>Теплоизоляция цоколя позволяет увеличить уровень комфорта в помещении за счет более равномерного прогрева полов и исключить промерзание конструкции. Кроме того, снижаются теплопотери через нижнюю часть здания;<br>
- <i class="icon-ok icon-md icon-color2"></i>Защищает цоколь от внешних атмосферных воздействий, продлевая срок службы всего здания;<br>
- <i class="icon-ok icon-md icon-color2"></i>Утеплённая отмостка уменьшает глубину промерзания, а следовательно снижает силы морозного пучения;<br>
- <i class="icon-ok icon-md icon-color2"></i>Отмостка позволяет отводить дождевую и талую воду от фундамента в дренажную системы.
-					</p>
- </li>
+						<div class="flexcol">
+ <span><i class="icon-ok icon-md icon-color2"></i><p>Теплоизоляция цоколя позволяет увеличить уровень комфорта в помещении за счёт более равномерного прогрева полов и исключить промерзание конструкции.</p></span>
+ <span><i class="icon-ok icon-md icon-color2"></i><p>Защищает цоколь от внешних атмосферных воздействий, продлевая срок службы всего здания;</p></span>
+ <span><i class="icon-ok icon-md icon-color2"></i><p>Утеплённая отмостка уменьшает глубину промерзания, а следовательно снижает силы морозного пучения;</p></span>
+ <span><i class="icon-ok icon-md icon-color2"></i><p>Отмостка позволяет отводить дождевую и талую воду от фундамента в дренажную системы.</p></span>
+						</div>
+					</li>
 					<li>
 					<div class="videoWrapper">
 						 <iframe width="640" height="480" src="//www.youtube.com/embed/lYnlh8QDE3I?rel=0&autoplay=0&showinfo=0&controls=0&HD=1" frameborder="0" allowfullscreen></iframe>
+						 <p>Смотрите ролик об утеплении цоколя</p>
 					</div>
- </li>
+					</li>
 				</ul>
 			</div>
 		</div>
 	</div>
 </div>
 </section>
+<div style="text-align: center">
+	<a href="https://www.stroyxpress.ru/catalog/uteplitel/ekstrudirovannyy_penopolistirol/" class="btn btn-primary btn-lg">Перейти в каталог</a>
+</div>
